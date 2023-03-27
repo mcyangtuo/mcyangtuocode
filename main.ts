ddc6d54534f4f5dca0fb14858c6282801e9b6bd3
mcyangtuo <86196177+mcyangtuo@users.noreply.github.com>
2023-03-27 21:50:27 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,73 @@
-player.onChat("run", function () {
-	
+player.onChat("刷石机32", function () {
+    for (let index = 0; index < 32; index++) {
+        agent.destroy(FORWARD)
+        agent.collectAll()
+        loops.pause(20)
+    }
+})
+player.onChat("挖矿16", function () {
+    for (let index = 0; index < 1; index++) {
+        agent.destroy(FORWARD)
+        agent.collectAll()
+        agent.move(FORWARD, 1)
+        agent.destroy(UP)
+        agent.collectAll()
+    }
+})
+player.onChat("tpc", function () {
+    agent.teleportToPlayer()
+})
+player.onChat("挖矿32", function () {
+    for (let index = 0; index < 32; index++) {
+        agent.destroy(FORWARD)
+        agent.collectAll()
+        agent.move(FORWARD, 1)
+        agent.destroy(UP)
+        agent.collectAll()
+    }
+})
+player.onChat("挖矿128", function () {
+    for (let index = 0; index < 128; index++) {
+        agent.destroy(FORWARD)
+        agent.collectAll()
+        agent.move(FORWARD, 1)
+        agent.destroy(UP)
+        agent.collectAll()
+    }
+})
+player.onChat("刷石机16", function () {
+    for (let index = 0; index < 16; index++) {
+        agent.destroy(FORWARD)
+        agent.collectAll()
+        loops.pause(20)
+    }
+})
+player.onChat("ai右", function () {
+    agent.turn(RIGHT_TURN)
+})
+player.onChat("挖矿64", function () {
+    for (let index = 0; index < 64; index++) {
+        agent.destroy(FORWARD)
+        agent.collectAll()
+        agent.move(FORWARD, 1)
+        agent.destroy(UP)
+        agent.collectAll()
+    }
+})
+player.onChat("ai左", function () {
+    agent.turn(LEFT_TURN)
+})
+player.onChat("刷石机128", function () {
+    for (let index = 0; index < 128; index++) {
+        agent.destroy(FORWARD)
+        agent.collectAll()
+        loops.pause(20)
+    }
+})
+player.onChat("刷石机64", function () {
+    for (let index = 0; index < 64; index++) {
+        agent.destroy(FORWARD)
+        agent.collectAll()
+        loops.pause(20)
+    }
 })

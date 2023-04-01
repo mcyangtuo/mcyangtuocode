1bcf78df8fee887179f99dd1218fbb23b95409b2
mcyangtuo <86196177+mcyangtuo@users.noreply.github.com>
2023-04-01 20:03:58 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ player.onChat("刷石机32", function () {
     for (let index = 0; index < 32; index++) {
         agent.destroy(FORWARD)
         agent.collectAll()
-        loops.pause(20)
+        loops.pause(60)
     }
 })
 player.onChat("挖矿16", function () {
@@ -44,7 +44,7 @@ player.onChat("刷石机16", function () {
     for (let index = 0; index < 16; index++) {
         agent.destroy(FORWARD)
         agent.collectAll()
-        loops.pause(20)
+        loops.pause(60)
     }
 })
 player.onChat("ai右", function () {
@@ -76,13 +76,15 @@ player.onChat("刷石机128", function () {
     for (let index = 0; index < 128; index++) {
         agent.destroy(FORWARD)
         agent.collectAll()
-        loops.pause(20)
+        loops.pause(60)
     }
 })
 player.onChat("刷石机64", function () {
     for (let index = 0; index < 64; index++) {
         agent.destroy(FORWARD)
         agent.collectAll()
-        loops.pause(20)
+        loops.pause(60)
     }
 })
+player.say("运行成功！欢迎使用mcyangtuocode！具体用途和聊天指令请查看代码内容和自行测试")
+player.say("提示：你可以自定义此代码参数来自定义您的游戏体验或者将这条提醒删除避免烦人")
